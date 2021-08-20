e8d80968009210a2db2e96596e11e3da051ede64
Raikish <albert.montagut.casero@gmail.com>
2021-08-20 16:31:24 +0200

    Infrastructure::Console

---
+++ Runtime/Infrastructure/Console.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Uno.Runtime.Infrastructure
+{
+    public class Console: MonoBehaviour
+    {
+        string characterRead;
+
+        #region Properties
+        public string CharacterRead
+        {
+            get => this.characterRead;
+        }
+        #endregion
+        
+        #region Constructors
+        public Console()
+        {
+            this.characterRead = "";
+        }
+        #endregion
+        
+        public void Write(string message)
+        {
+            Debug.Log(message);    
+        }
+
+        public IEnumerator Read()
+        {
+            yield return new WaitUntil(() => Input.anyKeyDown);
+            this.characterRead = Input.inputString;
+        }
+    }
+}
