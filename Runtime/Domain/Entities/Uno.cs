b1999ed459ab07860b677e3f3379405fb97009f5
Raikish <albert.montagut.casero@gmail.com>
2021-08-20 16:29:37 +0200

    Domain::Uno [WIP]

---
+++ Runtime/Domain/Entities/Uno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Uno.Runtime.Domain
+{
+    public class Uno
+    {
+        const int START_HAND_CARDS = 7;
+        
+        Turn turn;
+        Deck deck;
+
+        #region Constructors
+        public Uno(int numberOfPlayers, int humanPlayers)
+        {
+            deck = new UnoDeckFactory().Create();
+            turn = new Turn(CreatePlayers(numberOfPlayers, humanPlayers));
+        }
+        #endregion
+        
+        #region Support methods
+
+        private IEnumerable<Player> CreatePlayers(int numberOfPlayers, int humanPlayers)
+        {
+            if (deck == null)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            // TODO Add human and machine players and create them.
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                yield return new Player(GetPlayerStartingHand().ToList());
+            }
+        }
+
+        private IEnumerable<Card> GetPlayerStartingHand()
+        {
+            if (deck == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            for (int i = 0; i < START_HAND_CARDS; i++)
+            {
+                yield return deck.Draw();
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ Tests/Editor/Domain/UnoTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Uno.Tests.Editor.Domain
+{
+    public class UnoTests
+    {
+        [Test, Category("TODO"), Ignore("TODO")]
+        public void NumberOfPlayers_NewInstance_AmountOfPlayerIstheSame()
+        {
+            
+        }
+    }
+}
