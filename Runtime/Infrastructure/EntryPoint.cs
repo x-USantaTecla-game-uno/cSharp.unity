b02d85c0319cf2116e01f8732e04d59f97191dd4
Raikish <albert.montagut.casero@gmail.com>
2021-08-20 16:33:07 +0200

    Infrastructure::EntryPoint [WIP]

---
+++ Runtime/Infrastructure/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Uno.Runtime.Application;
+
+namespace Uno.Runtime.Infrastructure
+{
+    public class EntryPoint: MonoBehaviour
+    {
+        private void Awake()
+        {
+            var beginInputPort = new BeginInteractor();
+            var beginController = new BeginController(beginInputPort);
+            this.StartCoroutine(beginController.Begin());
+        }
+    }
+}
