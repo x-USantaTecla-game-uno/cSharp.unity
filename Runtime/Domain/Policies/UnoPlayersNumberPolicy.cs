a4ffdb51c98636bc1ab10a18b4c899b7192b7f30
Raikish <albert.montagut.casero@gmail.com>
2021-09-03 23:48:31 +0200

    Domain::UnoPlayersNumberPolicy

---
+++ Runtime/Domain/Policies/UnoPlayersNumberPolicy.cs
@@ -0,0 +1,13 @@
+﻿namespace Uno.Runtime.Domain
+{
+    public class UnoPlayersNumberPolicy
+    {
+        public const int MAX_PLAYER_NUMBER = 10;
+        public const int MIN_PLAYER_NUMBER = 2;
+        
+        public bool IsValidNumberOfPlayers(int numberOfPlayers)
+        {
+            return numberOfPlayers <= MAX_PLAYER_NUMBER && numberOfPlayers >= MIN_PLAYER_NUMBER;
+        }
+    }
+}
---
+++ Tests/Editor/Domain/UnoPlayersNumberPolicyTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Uno.Runtime.Domain;
+
+namespace Uno.Tests.Editor.Domain
+{
+    public class UnoPlayersNumberPolicyTests
+    {
+        [Theory]
+        public void MinNumberOfPlayers_Validates_IsValidInput()
+        {
+            var sut = new UnoPlayersNumberPolicy();
+
+            var result = sut.IsValidNumberOfPlayers(UnoPlayersNumberPolicy.MIN_PLAYER_NUMBER);
+
+            result.Should().BeTrue();
+        }
+        
+        [Theory]
+        public void MaxNumberOfPlayers_Validates_IsValidInput()
+        {
+            var sut = new UnoPlayersNumberPolicy();
+
+            var result = sut.IsValidNumberOfPlayers(UnoPlayersNumberPolicy.MAX_PLAYER_NUMBER);
+
+            result.Should().BeTrue();
+        }
+        
+        [Theory]
+        public void OneLessThanMinNumberOfPlayers_Validates_IsNotValidInput()
+        {
+            var sut = new UnoPlayersNumberPolicy();
+
+            var result = sut.IsValidNumberOfPlayers(UnoPlayersNumberPolicy.MIN_PLAYER_NUMBER - 1);
+
+            result.Should().BeFalse();
+        }
+        
+        [Theory]
+        public void OneMoreThanMaxNumberOfPlayers_Validates_IsNotValidInput()
+        {
+            var sut = new UnoPlayersNumberPolicy();
+
+            var result = sut.IsValidNumberOfPlayers(UnoPlayersNumberPolicy.MAX_PLAYER_NUMBER + 1);
+
+            result.Should().BeFalse();
+        }
+    }
+}
