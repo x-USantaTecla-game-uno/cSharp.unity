c56b80b3b20095ee4b31a22dabeab16caa395775
Raikish <albert.montagut.casero@gmail.com>
2021-09-04 11:08:27 +0200

    Application::BeginView

---
+++ Runtime/Application/BeginView.cs
@@ -0,0 +1,10 @@
+﻿using Uno.Runtime.Application;
+
+namespace Uno.Runtime.Infrastructure
+{
+    public interface BeginView
+    {
+        void NoticeWantNumberOfPlayers(NotificationViewModel viewModel);
+        void NoticeWantNumberOfHumans(NotificationViewModel viewModel);
+    }
+}
---
+++ Runtime/Application/Contracts/NotificationViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Uno.Runtime.Application
+{
+    public class NotificationViewModel
+    {
+        public string Message
+        {
+            get;
+        }
+        
+        #region Constructors
+        public NotificationViewModel(string message)
+        {
+            Message = message;
+        }
+        #endregion
+    }
+}
