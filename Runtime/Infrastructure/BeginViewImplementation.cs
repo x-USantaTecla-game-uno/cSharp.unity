14a1b6a3ee2c8fbfdc4913d5615c637080aeb1b6
Raikish <albert.montagut.casero@gmail.com>
2021-09-04 11:08:47 +0200

    Infrastructure::BeginViewImplementation

---
+++ Runtime/Infrastructure/BeginViewImplementation.cs
@@ -0,0 +1,26 @@
+﻿using Uno.Runtime.Application;
+
+namespace Uno.Runtime.Infrastructure
+{
+    public class BeginViewImplementation : BeginView
+    {
+        private readonly Console console;
+        
+        #region Constructor
+        public BeginViewImplementation(Console console)
+        {
+            this.console = console;
+        }
+        #endregion
+        
+        public void NoticeWantNumberOfPlayers(NotificationViewModel viewModel)
+        {
+            console.Write(viewModel.Message);
+        }
+
+        public void NoticeWantNumberOfHumans(NotificationViewModel viewModel)
+        {
+            console.Write(viewModel.Message);
+        }
+    }
+}
